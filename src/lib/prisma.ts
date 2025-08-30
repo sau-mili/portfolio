34dd939a8bb83845d2001887ac4048d3c021eb99
sau-mili <146661411+sau-mili@users.noreply.github.com>
2025-08-30 23:50:28 +0530

    small change

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,14 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
 export const prisma =
-  globalForPrisma.prisma ||
+  global.prisma ??
   new PrismaClient({
-    log: ["query"],
+    log: ["query"], // optional, helpful for debugging
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
